f89d6404f9bb2a4a304b361d116b657b925ecf68
Wesley Chong <cckwes@gmail.com>
2022-01-22 19:28:45 +0800

    install packages and setup main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import helmet from "helmet";
+
+import { version } from "../package.json";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+
+  if (process.env.NODE_ENV === "development") {
+    const config = new DocumentBuilder()
+      .setTitle("tmai")
+      .setDescription("API document for tmai-server")
+      .setVersion(version)
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
