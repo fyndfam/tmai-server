0cd9163c4bd5b7c734fd36b34dd3c579718b34cb
Wesley Chong <cckwes@gmail.com>
2022-01-30 13:50:46 +0800

    Add health check endpoint (#3)
    * add health check endpoint
    
    * bump version

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { getMongoConnectionOptions } from "./shared/db";
 import { PostModule } from "./post/post.module";
 import { RequestLoggingMiddleware } from "./shared/request-logging.middleware";
+import { HealthCheckModule } from "./health-check/health-check.module";
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL, getMongoConnectionOptions()),
     TerminusModule,
+    HealthCheckModule,
     PostModule,
   ],
   controllers: [],
