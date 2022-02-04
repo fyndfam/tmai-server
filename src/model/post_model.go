e4144591f8e488d9cc8767eabae365bb298e2e5e
Wesley Chong <cckwes@gmail.com>
2022-02-04 23:13:18 +0800

    add post model

---
+++ src/model/post_model.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type PostModel struct {
+	ID            primitive.ObjectID `json:"id" bson:"_id"`
+	Content       string             `json:"content" bson:"content"`
+	Tags          []string           `json:"tags,omitempty" bson:"tags,omitempty"`
+	View          int64              `json:"view" bson:"view"`
+	CreatedBy     primitive.ObjectID `json:"createdBy" bson:"createdBy"`
+	ContentEdited bool               `json:"contentEdited" bson:"contentEdited"`
+	CreatedAt     time.Time          `json:"createdAt" bson:"createdAt"`
+	UpdatedAt     time.Time          `json:"updatedAt" bson:"updatedAt"`
+}
