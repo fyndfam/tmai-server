de59a40fe8fabf31442bfba0a57f86bc69c25121
Wesley Chong <cckwes@gmail.com>
2022-03-12 12:20:11 +0800

    enable cors

--- src/server/server.go
+++ src/server/server.go
@@ -4,10 +4,13 @@ import (
 	"github.com/fyndfam/tmai-server/src/controller"
 	"github.com/fyndfam/tmai-server/src/env"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func NewApp(env *env.Env) *fiber.App {
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	controller.MountHealthCheckRoutes(env, app)
 	controller.MountUserRoutes(env, app)
